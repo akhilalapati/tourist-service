5d217f2a1daa9d21536321b944b41f4959917064
akhil_alapati <akhil.alapati@signiasoftware.com>
2023-07-08 16:33:38 +0530

    cross origin

--- src/main/java/com/example/tourist/Controller/TouristController.java
+++ src/main/java/com/example/tourist/Controller/TouristController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = {"http://localhost:4200", "https://tourist-ui.herokuapp.com/"})
+@CrossOrigin(origins = {"http://localhost:4200", "https://tourist-ui.herokuapp.com/", "https://tourist-user-interface.netlify.app/"})
 public class TouristController {
 
     @Autowired
